70b186a7e2159130385d7884b568ac2ff4db0f5f
dhiaurrahmaamelia <dhiaurrahmaamelia2024@gmail.com>
2025-01-08 11:33:43 +0700

    Add files via upload

---
+++ variabel.js
@@ -0,0 +1,42 @@
+let nama="Dhiaurrahma amelia baqir";
+console.log(nama);
+
+let alamat="Boko payung,Tugu,Sayung,Demak";
+console.log(alamat);
+
+let usia ="16 Tahun";
+console.log(usia);
+
+let TTL="Demak,2, januari,2009";
+console(TTL);
+
+let gender="wanita";
+console.log(gender);
+
+let agama="islam";
+console.log(agama);
+
+let email="Dhiaurrahma amelia2024@gmail.com";
+console.log(email);
+
+let berat="35";
+console.log(berat);
+
+let tinggi="156";
+console.log(tinggi);
+let hobi="";
+console.log(hobi);
+
+let cita="menjadi pengusaha sukses";
+console.log(cita);
+
+let setatus="pelajar";
+console.log(setatus);
+
+let sekolah="SMK N 1 SAYUNG";
+console.log(sekolah);
+
+let jurusan="pplg 2";
+console.log(jurusan);
+ 
+ document.writer (" nama saya adalah " + nama + alamat +  " alamat saya adalah " + usia + " usia saya adalah " + TTL + " TTL saya adalah " + Gender + " Gender saya adalah " + agama + " agama saya adalah " + email + " email saya adalah " + berat + " berat saya adalah " + tinggi + " tinggi saya adalah " + hobi + " hobi saya adalah " + cita + " cita saya adalah " + setatus + " setatus saya adalah " + sekolah + " sekolah saya adalah " + jurusan + " jurusan saya adalah ")
